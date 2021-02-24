bb233230b3f1e7692f417a844cf4a789c6f53a67
gpjalocha <gpjalocha@gmail.com>
2021-02-24 12:15:31 +0100

    added 3d wrapper for obj

---
+++ obj_3dmodel_wrapper/README.md
@@ -0,0 +1,57 @@
+# OBJ to XML _<3dmodel../>_ wrapper
+
+This scripts allow you to convert OBJ and MTL exported models from blender to xml `<3dmodel...>` code. It separates exported model into batches by assigned materials. It also suppors assigning specific textures and materials by specifying those in material names in blender. This script also extracts UV data from OBJ so you can map the texture on your model in blender's UV editor.
+
+## Prerequisite
+
+* Model needs to be triangulated before export(more about triangulation in next paragraph)
+* To extract colours model must have assigned material(for example principled BSDF) with  base color from which converter extracts HEX
+* If you specify `Textures\\<texture_name>.png`or/and `Materials\\<material_name>.xml`, then converter extracts those from material name and assigns those to the batch.
+
+## Model trangulation
+Before exporting your model it needs to be triangulated. You can do that in 3 ways:
+
+1. Select *Triangulate Faces* checkbox in export menu under *Geometry* tab:
+![trian](trian.jpg)
+
+2. Apply *Decimate* modifier with *triangulate* option. It's the best way if you have model with big vertices count, so you can compress it before exporting to XML
+![decimate](decimate.jpg)
+
+3. Apply *Triangulate* modifier.
+
+## Setting up the materials and textures
+You can assign the Texture and Material in material name. If it's not specified correctly(or not specified at all) then it assigns by default `Material\\material1.xml` and `Textures\\concrete5.png`.
+![material](material.jpg)
+
+## Setting up the UV mapping
+If you create some model from scratch, it's good to check if UV mapping is correct. You can check that in UV editor while being in Edit Mode(Tab) and selecting all the object vertices(a).
+
+## Export instruction
+* Make sure, that you have your model selected.
+*tip*: If you have multiple objects, you can merge them into one by selecting all of them in object mode, and then press *ctrl+j*
+* File->Export->Wavefront(.obj)
+* Select *Selection only* checkbox and set *Z forward* in transform panel
+* Export to the folder where the script is located 
+
+## Usage
+
+Script require following arguments:
+--input_obj=cube --output_xml=cube --inv_faces=0 --scale_uv=1 --model_tag=3dmodel
+* input_obj - name of the obj file(without extension)
+* output_xml- name of the output file(without extension- it's xml)
+* inv_faces- flag which defines if faces should be inverted. If you have model with wrong topology and in DSJ model is fliped(like pillar with lz-rz flipped) then setting this flag to 1 should help
+* scale_uv- scale factor for UV mapping. If you have UV mapping well defined in blender, then it should be 1. For value bigger than 1 texture mapped is bigger, for values smaller than 1 texture is smaller.
+* model_tag- tag for output xml `<3dmodel`. If your hill is in 1.8.0 version, then you should set this value to `model`
+
+Example usage: navigate to this folder and launch command below:
+```{bash}
+python obj_wrapper_3d.py\
+--input_obj=cube \
+--output_xml=cube \
+--inv_faces=0 \
+--scale_uv=1 \
+--model_tag=3dmodel
+```
+
+After executing that command, cube.xml file should appear. Copy the content of this file into your hill xml and it should result with this.
+![res](res.jpg)
---
+++ obj_3dmodel_wrapper/obj_wrapper_3d.py
@@ -0,0 +1,155 @@
+#!/bin/python
+
+import numpy as np
+from re import findall,match,search
+import sys
+
+#read input
+
+input={}
+keys = ["--model_tag=","--input_obj=","--output_xml=","--inv_faces=","--scale_uv="]
+for i in range(1,len(sys.argv)):                                                                                                       
+    for key in keys:                                                                                                                   
+        if sys.argv[i].find(key) == 0:                                        
+            if sys.argv[i][2:(len(key)-1)] in ['model_tag','input_obj','output_xml']:
+                input[sys.argv[i][2:(len(key)-1)]]=str(sys.argv[i][len(key):])
+            else:
+                input[sys.argv[i][2:(len(key)-1)]]=float(sys.argv[i][len(key):])
+            break
+
+
+def read_obj(path="./models/windmill.obj"):
+    f=open(path,'r')
+    lines=f.readlines()
+    v=[]
+    vt=[]
+    f={}
+    iv=1
+    for line in lines:
+        if(bool(match("^v ",line))):
+                v=v+[[float(x) for x in findall('-?[\d.]+',line)]]
+        if(bool(match("^vt ",line))):
+                vt=vt+[[float(x) for x in findall('-?[\d.]+',line)]]
+        if(bool(match("usemtl",line))):
+                material=search("usemtl (.+)",line).group(1)
+                f[material]={}
+                f[material]['v']=[]
+                f[material]['uv']=[]
+        if(bool(match("^f ",line))):
+                f[material]['v']+=[[int(x) for x in findall(' ([\d.]+)/',line)]]
+                f[material]['uv']+=[[int(x) for x in findall(' [\d.]+?/([\d.]+)',line)]]
+    return v,f,vt
+
+def blend(color, alpha, base=[255,255,255]):
+    out = [int(round((alpha * color[i]) + ((1 - alpha) * base[i]))) for i in range(3)]
+    return out
+
+def to_hex(color):
+    return ''.join(["%02x" % e for e in color])
+
+def read_mtl(path="./models/windmill.mtl"):
+    f=open(path,'r')
+    lines=f.readlines()
+    v=[]
+    f={}
+    iv=1
+    for line in lines:
+        if(bool(match("newmtl",line))):
+                material=search("newmtl (.+)",line).group(1)
+                f[material]=''
+        if(bool(match("^Kd ",line))):
+                f[material]+=to_hex(blend([float(x)*255 for x in findall('([\d.]+)',line)],1))
+    return f
+
+def extract_texture(input_str):
+    if(bool(match(r".*Textures\\.+?\.png",input_str))):
+        parsed_texture=match(r".*(Textures\\.+?\.png)",input_str).group(1)
+        return(parsed_texture)
+    else:
+        return("Textures\\concrete5.png")
+
+def extract_material(input_str):
+    if(bool(match(r".*Materials\\.+?\.xml",input_str))):
+        parsed_material=match(r".*?(Materials\\.+?\.xml)",input_str).group(1)
+        return(parsed_material)
+    else:
+        return("Materials\\material1.xml")
+
+def convert_3d(name,v,f,vt,mtl,scale_uv,invert_faces,model_tag):
+    if model_tag=="3dmodel":
+        prefix="3d"
+    else:
+        prefix=""
+    command = ('<%smodel id="'+name+'">\n\t') % prefix
+    for batch in f:
+            verts_str=[]
+            faces=[]
+            verts_all=[]
+            color=mtl[batch]
+            material=extract_material(batch)
+            texture=extract_texture(batch)
+            command+="<batch id=\""+batch+'" texture1="'+texture+'" zbias="3" material="'+material+'" fvf="322" order="0">\n\t\t'
+            kl=0
+            allVertsUv=[]
+            print("processing texture batch name: %s\nmaterial: %s\ntexture: %s\ncolor=%s\n" %tuple([batch,material,texture,color]))
+            for face_v,face_uv in zip(f[batch]['v'],f[batch]['uv']):
+                allVertsUv+=[str(a-1)+'_'+str(b-1) for a,b in zip(face_v,face_uv)]
+            allVertsUv=list(set(allVertsUv))
+            allVertsTableId=dict(zip(allVertsUv,range(len(allVertsUv))))
+            for face_v,face_uv in zip(f[batch]['v'],f[batch]['uv']):
+                    if len(face_v)==3:
+                            vertsInd=[i-1 for i in face_v]
+                            verts=[v[int(i)-1] for i in face_v]
+                            uvInd=[i-1 for i in face_uv]
+                            uv=[vt[int(i)-1] for i in face_uv]
+                            for Ind,vertex,UVind,UV in zip(vertsInd,verts,uvInd,uv):
+                                    kl+=1
+                                    vertVal=str(Ind)+'_'+str(UVind)
+                                    INDEX=allVertsTableId[vertVal]
+                                    vert_vals=[float(vr) for vr in vertex]
+                                    vert_vals[2]=-vert_vals[2]
+                                    if (Ind,UVind) not in verts_all:
+                                            verts_str+=[('<vertex id="%i" x="%0.4f" y="%0.4f" z="%0.4f" u1="%0.4f" v1="%0.4f" diffuse="0x'+material[1]+'%s"/>\n\t\t') % tuple([INDEX]+vert_vals+[UV[0]/scale_uv,-UV[1]/scale_uv]+[color])]
+                                            verts_all+=[tuple([Ind,UVind])]
+                            if(invert_faces == 0):
+                                faces+=['\n\t\t<face v2="%i" v1="%i" v3="%i"/>' % tuple([allVertsTableId[str(a)+'_'+str(b)] for a,b in zip(vertsInd,uvInd)])]
+                            else:
+                                faces+=['\n\t\t<face v1="%i" v2="%i" v3="%i"/>' % tuple([allVertsTableId[str(a)+'_'+str(b)] for a,b in zip(vertsInd,uvInd)])]
+                    else:
+                        print("Detected face with more than 3 vertices:\n")
+                        print(face_v)
+                        print('\n\n Triangulate your model!')
+                        quit()
+            verts_numbs=[int(findall(' id="(.+?)"',j)[0]) for j in verts_str]
+            zipped_verts=list(set(zip(verts_numbs,verts_str)))
+            sorted_verts=sorted(zipped_verts)
+            verts_str=[x for _,x in sorted_verts]
+            faces_numbs=[int(findall(' v1="(.+?)"',j)[0]) for j in faces]
+            zipped_faces=zip(faces_numbs,faces)
+            sorted_faces=sorted(zipped_faces)
+            faces=[x for _,x in sorted_faces]
+            command+=''.join(verts_str)+'\n\n'+''.join(faces)
+            command+='\n\t</batch>\n\t'
+    command+=("\n</%smodel>\n\n<%smodel-instance id=\""+name+"\"/>") % tuple([prefix,prefix])
+    return(command)
+
+def save_to_file(io,path):
+    file=open(path+'.xml','w')
+    file.write(io)
+    file.close()
+
+
+
+    # execute only if run as a script
+model=input['input_obj']
+output=input['output_xml']
+scale_uv=input['scale_uv']
+invert_faces=input['inv_faces']
+model_tag=input['model_tag']
+
+v,f,vt=read_obj(model+'.obj')
+mtl=read_mtl(model+'.mtl')
+res=convert_3d(model,v,f,vt,mtl,scale_uv,invert_faces,model_tag)
+
+save_to_file(res,output)
+print("Conversion finished, output file:\n"+output+".xml")
