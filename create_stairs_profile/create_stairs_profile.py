7b024056f172808588fa5ab968c47f6c5d72aaec
gpjalocha <gpjalocha@gmail.com>
2021-11-06 22:59:56 +0100

    added input conditions and warnings

--- create_stairs_profile/create_stairs_profile.py
+++ create_stairs_profile/create_stairs_profile.py
@@ -14,6 +14,33 @@ for i in range(1,len(sys.argv)):
                 input[sys.argv[i][2:(len(key)-1)]]=float(sys.argv[i][len(key):])
             break
 
+def is_input_valid(input):
+    if(input['start']>input['end']):
+        print("ERROR: end should have bigger value than start")
+        sys.exit()
+    if(input['step']<0):
+        print("ERROR: step should be bigger than zero")
+        sys.exit()
+    if(input['offset']<0):
+        print("ERROR: offset should be bigger than zero")
+        sys.exit()
+    if(input['offset']>input['step']):
+        print("ERROR: offset should be smaller than the step")
+        sys.exit()
+
+def print_warnings(input):
+    if(input['refx'] not in ['inrun','dhill']):
+        print("WARNING: Using custom refx:\"%s\". Make sure you have this refx defined in your hill xml" % input['refx'])
+    if(input['refy'] not in ['inrun-top','dhill-top']):
+        print("WARNING: Using custom refy:\"%s\". Make sure you have this refy defined in your hill xml" % input['refy'])
+    if(input['offset']>.05):
+        print("WARNING: offset is quite big. It's ok if the stairs are supposed to be thin. But if not, consider using a smaller value")
+    if(input['h']<0):
+        print("WARNING: height of the steps is less than zero. It might be intended to put the stairs below the profile, but this might be wrong.")
+
+is_input_valid(input)
+print_warnings(input)
+
 profile_str=     '<!--STAIRS PROFILE-->\n\n'
 profile_str+=     '<profile id="%s-stairs" refx="%s">\n\t' % tuple([input['refx'],input['refx']])
 profile_str+=    '<start x="%0.3f" y="%0.3f" refy="%s"/>\n\t' % tuple([input['start'],input['h'],input['refy']])
